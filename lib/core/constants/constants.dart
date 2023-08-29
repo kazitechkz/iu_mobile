2b39e83a8c478a20e95854a2bf81e046dc1f116f
idlkz <idlkz2019@gmail.com>
2023-08-29 12:09:12 +0600

    Commit assets/bears images

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,20 @@
+class BearImages {
+  static const String TOURNAMENT = 'assets/bears/IMG_7357.jpeg';
+  static const String IMG_7358 = 'assets/bears/IMG_7358.jpeg';
+  static const String IMG_7359 = 'assets/bears/IMG_7359.jpeg';
+  static const String WORLD_HISTORY = 'assets/bears/IMG_7360.jpeg';
+  static const String KAZAKH_LANGUAGE = 'assets/bears/IMG_7361.jpeg';
+  static const String FUNDAMENTALS_OF_RIGHT = 'assets/bears/IMG_7362.jpeg';
+  static const String RUSSIAN_LITERATURE = 'assets/bears/IMG_7363.jpeg';
+  static const String ENGLISH_LANGUAGE = 'assets/bears/IMG_7364.jpeg';
+  static const String BIOLOGY = 'assets/bears/IMG_7365.jpeg';
+  static const String THINKING_BEAR = 'assets/bears/IMG_7366.jpeg';
+  static const String CHEMISTRY = 'assets/bears/IMG_7367.jpeg';
+  static const String KAZAKH_HISTORY = 'assets/bears/IMG_7368.jpeg';
+  static const String MATHEMATICS = 'assets/bears/IMG_7369.jpeg';
+  static const String FHYSICS = 'assets/bears/IMG_7370.jpeg';
+  static const String FLYING_BEAR = 'assets/bears/IMG_7371.jpeg';
+  static const String HELLO_BEAR = 'assets/bears/IMG_7372.jpeg';
+  static const String COMPUTER_SCIENCE = 'assets/bears/IMG_7373.jpeg';
+  static const String IMG_7375 = 'assets/bears/IMG_7375.jpeg';
+}
