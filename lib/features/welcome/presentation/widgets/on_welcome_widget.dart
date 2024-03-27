2e86a221baf786815a21bd012627654f77e96c81
idlkz <idlkz2019@gmail.com>
2024-03-27 17:48:34 +0500

    Commit welcome_screen

--- lib/features/welcome/presentation/widgets/on_welcome_widget.dart
+++ lib/features/welcome/presentation/widgets/on_welcome_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import '../../../../core/app_constants/color_constant.dart';
 import '../../domain/entities/WelcomeEntity.dart';
 import '../bloc/welcome_bloc.dart';
@@ -15,63 +16,74 @@ class OnWelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Padding(
-          padding: EdgeInsets.only(bottom: 20.h ),
-          child: Image(image: AssetImage(data.image),height: 250.h),
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Image.asset(data.image, fit: BoxFit.contain,),
         ),
-        Padding(
-            padding: EdgeInsets.only(left: 20.w,right: 20.w),
-          child: Column(
-            children: [
-              Text(
-              data.title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                  fontSize: 32
-              ),
+        SizedBox(height: 10.h,),
+        Text(data.title, style: TextStyle(
+            color: ColorConstant.mainDarkColor,
+            fontSize: 28.sp,
+            fontWeight: FontWeight.normal
+        ),),
+        SizedBox(height: 10.h,),
+        Container(
+          alignment: Alignment.center,
+          width: 375.w,
+          padding: EdgeInsets.symmetric(horizontal: 30.w),
+          child: Text(data.description, textAlign: TextAlign.center, style: TextStyle(
+              color: ColorConstant.grayDarkColor,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.normal
+          ),),
+        ),
+        SizedBox(height: 70.h,),
+        GestureDetector(
+          onTap: () {
+            if(isLast){
+              context.read<WelcomeBloc>().add(const WelcomeCacheEvent());
+            }
+            else{
+              pageController.animateToPage(index, duration: const Duration(milliseconds: 700), curve: Curves.easeInOut);
+            }
+          },
+          child: CircularPercentIndicator(
+            radius: 70.0,
+            lineWidth: 7.0,
+            animation: true,
+            percent: 0.33,
+            center: Container(
+              child: Text('Next'),
             ),
-              SizedBox(height: 20.h),
-              Text(
-                data.description,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black54,
-                    fontSize: 22
-                ),
-              ),
-              SizedBox(height: 20.h,),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
-                  backgroundColor: ColorConstant.violetFirst
-                ),
-                  onPressed: (){
-                  if(isLast){
-                    context.read<WelcomeBloc>().add(const WelcomeCacheEvent());
-                  }
-                  else{
-                    pageController.animateToPage(index, duration: const Duration(milliseconds: 700), curve: Curves.easeInOut);
-                  }
-                  },
-                  child: Text(
-                    ( isLast == true ? "Приступить  к работе" : "Далее"),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 25
-                    ),
-                  )
-
-              )
-
-            ],
+            circularStrokeCap: CircularStrokeCap.round,
+            progressColor: ColorConstant.orangeColorDark,
           ),
-
+          // child: Container(
+          //   margin: EdgeInsets.only(top: 150.h, left: 25.w, right: 25.w),
+          //   width: 325.w,
+          //   height: 50.h,
+          //   decoration: BoxDecoration(
+          //       color: ColorConstant.violetFirst,
+          //       borderRadius: BorderRadius.all(Radius.circular(15.w)),
+          //       boxShadow: [
+          //         BoxShadow(
+          //             color: Colors.grey.withOpacity(0.1),
+          //             spreadRadius: 1,
+          //             blurRadius: 2,
+          //             offset: const Offset(0, 1)
+          //         )
+          //       ]
+          //   ),
+          //   child: Center(
+          //     child: Text(( isLast == true ? "Приступить  к работе" : "Далее"), style: TextStyle(
+          //         color: Colors.white,
+          //         fontSize: 16.sp,
+          //         fontWeight: FontWeight.normal
+          //     )),
+          //   ),
+          // ),
         )
       ],
     );
