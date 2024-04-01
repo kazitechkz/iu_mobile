44ca0f5e5195a2c7ffb428524550e141d99ba39e
IDLKZ <idlkz2019@gmail.com>
2024-04-01 15:07:19 +0500

    Added MiddleWare and Bearer Interceptor and 401 Logic

---
+++ lib/core/common/widgets/bottom_nav_bar.dart
@@ -0,0 +1,80 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:iu/core/app_constants/color_constant.dart';
+import 'package:iu/features/dashboard/presentation/dashboard_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> with TickerProviderStateMixin{
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBottomNavigationBar.builder(
+        height: 60.w,
+        tabBuilder: (int index, bool isActive) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+              iconList[index],
+              size: 20,
+              color: isActive ? ColorConstant.violetFirst : ColorConstant.borderGrayColor,
+              ),
+              Text(
+                  iconLabel[index],
+                  style: TextStyle(
+                    fontSize: 10.sp,
+                    color: isActive ? ColorConstant.violetFirst : ColorConstant.borderGrayColor,
+                  ),
+              )
+            ],
+          );
+        },
+      elevation: 12,
+      activeIndex: 0,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.verySmoothEdge,
+      leftCornerRadius: 24,
+      rightCornerRadius: 24,
+      onTap: (index) => {},
+      itemCount: iconList.length,
+      //other params
+    );
+  }
+
+  List<IconData> iconList = [
+    FontAwesomeIcons.house,
+    FontAwesomeIcons.book,
+    FontAwesomeIcons.wandSparkles,
+    FontAwesomeIcons.user,
+  ];
+  List<String> iconLabel = [
+    "Главная",
+    "Обучение",
+    "Battle",
+    "Профиль"
+  ];
+
+
+
+
+}
+
--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iu/core/providers/user_provider.dart';
 
+import '../../../core/common/widgets/bottom_nav_bar.dart';
+
 class DashBoardScreen extends StatelessWidget {
   const DashBoardScreen({super.key});
 
@@ -11,6 +13,12 @@ class DashBoardScreen extends StatelessWidget {
       body: Center(
         child: Text("Dashboard ${context.read<UserProvider>().user?.name}"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+      //params
+     ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
